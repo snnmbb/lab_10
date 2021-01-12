a2e6260c7ecc55ab0ab4cec305088e2ee9eafc36
Nunnalin Bingbing <61963924+snnmbb@users.noreply.github.com>
2021-01-12 15:42:55 +0700

    finished

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,27 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    
+    int i;
+    i=rand()%9;
+    string grade[9] ={"A","B+","B","C+","C","D+","D","F","W"};
+    
+    cout<<"You will get "<< grade[i] <<" in this 261102.";
+}
+
+
+
+
+
+
